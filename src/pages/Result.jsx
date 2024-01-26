ee4cc1779a96536f6375cc1d61564d4534da9847
ahmdali25 <ahmadaliazhary@gmail.com>
2024-01-26 11:17:50 +0700

    ADD: add state if there are no movie search results

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -4,6 +4,7 @@ import { MovieSearch } from "../context/MovieSearch";
 
 export default function Result() {
   const { query, movie } = useContext(MovieSearch);
+
   return (
     <section>
       <div className="flex items-center gap-4">
@@ -11,19 +12,24 @@ export default function Result() {
           Result for : {query}
         </h1>
       </div>
-      <div className="mt-4 grid grid-cols-2 gap-10 md:grid-cols-3 lg:grid-cols-5">
-        {movie.map((movie) => (
-          <MovieCard
-            key={movie.id}
-            id={movie.id}
-            title={movie.title || movie.name}
-            poster={movie.poster_path}
-            rating={movie.vote_average}
-            releaseDate={movie.release_date || movie.first_air_date}
-            path={movie.title ? "movie-detail" : "tv-detail"}
-          ></MovieCard>
-        ))}
-      </div>
+
+      {movie && movie.length > 0 ? (
+        <div className="mt-4 grid grid-cols-2 gap-10 md:grid-cols-3 lg:grid-cols-5">
+          {movie.map((movie) => (
+            <MovieCard
+              key={movie.id}
+              id={movie.id}
+              title={movie.title || movie.name}
+              poster={movie.poster_path}
+              rating={movie.vote_average}
+              releaseDate={movie.release_date || movie.first_air_date}
+              path={movie.title ? "movie-detail" : "tv-detail"}
+            ></MovieCard>
+          ))}
+        </div>
+      ) : (
+        <p className="mt-4">Tidak ada film yang ditemukan.</p>
+      )}
     </section>
   );
 }
